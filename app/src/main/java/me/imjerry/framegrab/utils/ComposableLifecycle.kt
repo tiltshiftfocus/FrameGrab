8a92d5d1bc538be30f6bfcb93d16af3c0a170c94
Jerry <limzming@gmail.com>
2024-01-20 18:09:47 +0800

    ComposableLifecycle for handling app lifecycle

---
+++ app/src/main/java/me/imjerry/framegrab/utils/ComposableLifecycle.kt
@@ -0,0 +1,27 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+
+@Composable
+fun ComposableLifecycle(
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+    onPause: () -> Unit = {}
+) {
+
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { source, event ->
+            when (event) {
+                Lifecycle.Event.ON_PAUSE -> onPause()
+                else -> {}
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+}
